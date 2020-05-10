77ca697322278a893c252498f1063cee2abac4f2
Rachel Bolton <rachelbolton@Rachels-MacBook-Air.local>
2020-05-10 15:43:53 +0200

    Get rid of old files

--- checker_push_swap.h
+++ checker_push_swap.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   checker_push_swap.h                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rbolton <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: rachelbolton <rachelbolton@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/07 18:27:15 by rbolton           #+#    #+#             */
-/*   Updated: 2019/08/20 18:29:43 by rbolton          ###   ########.fr       */
+/*   Updated: 2020/05/10 15:42:37 by rachelbolto      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,7 @@ typedef struct		s_stack
 
 int					atoi_check(char **array);
 int					all_int(char **array);
-t_node				*create_stack(char **array);
+t_node				*create_stack(char *array);
 t_node				*push(t_node *head, int data);
 t_node				*pop(t_node *head, int *element); //to use this meaningfully: when calling pop(), ensure that a pointer to int exists. That pointer will contain the popped data after the function call which can then be used to either compare or push() to another stack.
 int					is_empty(t_stack *head);
