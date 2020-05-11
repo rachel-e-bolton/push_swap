a665499cdd4281ee521fbc9d0ba84ddd540abc45
Rachel Bolton <rachelbolton@Rachels-MacBook-Air.local>
2020-05-11 19:58:20 +0200

    Handles multiple args and single string arg

--- checker_src/checker_main.c
+++ checker_src/checker_main.c
@@ -6,7 +6,7 @@
 /*   By: rbolton <rbolton@student.wethinkcode.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/10 15:51:50 by rbolton           #+#    #+#             */
-/*   Updated: 2020/05/11 11:36:08 by rbolton          ###   ########.fr       */
+/*   Updated: 2020/05/11 19:57:21 by rbolton          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,26 @@
 int main (int ac, char **av) 
 {
   t_int_array *int_array = NULL;
+  char *args = NULL;
 
   if (ac == 2) // We expect a single string as argument. Only reading av[1]. No flags
   {
     int_array = valid_input_check(av[1]);
-    while (int_array->len--)
-    {
-      printf("%d\n", *int_array->array);
-      ++int_array->array;
-    }
   } 
   else if (ac > 2) // We expect more than one string as argument. Valid flags: -v and -c. Aim for any order. Any string not starting with "-" we can assume to be a candidate for our args and the args test gets performed on this.
-  {}
+  {
+    args = implode_args(av, ac);
+    int_array = valid_input_check(args);
+  }
   else
   {
     ft_putstr_fd("Error\n", 2);
     exit(1);
   }
+  while (int_array->len--)
+  {
+    printf("%d\n", *int_array->array);
+    ++int_array->array;
+  }
+  free(int_array->array);
 }
